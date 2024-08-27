3ef0229816ca12c7eac96cc255833aa431f49270
Yobel Sahala <34935946+aGlassofMarjan@users.noreply.github.com>
2024-08-27 12:04:06 +0700

    feat: init project

---
+++ place-order/src/main/java/com/widetechnologies/place_order/entity/OrderItem.java
@@ -0,0 +1,20 @@
+package com.widetechnologies.place_order.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Product product;
+
+    private int quantity;
+    private double totalPrice;
+}
